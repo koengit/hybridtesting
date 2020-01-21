993269d5c0da3b7c790ebd0470bf5748b63a2bb8
Koen Claessen <koen@chalmers.se>
2020-01-21 16:43:13 +0100

    now the spaceship also works

--- examples/SpaceShip.hs
+++ examples/SpaceShip.hs
@@ -17,7 +17,7 @@ check =
   sequential skip (var position >=? 100) $
     first (assert "reached destination" false)
 
-test :: Valued f => [Double] -> f ([Env], Result)
+test :: Valued f => [Double] -> [Env f]
 test vals = simulate 1 envs (lower stdPrims $ ship & check)
   where
-    envs = [Map.singleton acceleration (DoubleValue x) | x <- vals]
+    envs = [Map.singleton acceleration (val (DoubleValue x)) | x <- vals]
