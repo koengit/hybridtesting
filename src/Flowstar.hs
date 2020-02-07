812dbaf53360d906950724187723cfcab3239df7
Nick Smallbone <nick@smallbone.se>
2020-02-07 15:06:53 +0100

    Add function for running flowstar with Nelder-Meade.

--- src/Flowstar.hs
+++ src/Flowstar.hs
@@ -3,8 +3,15 @@ module Flowstar where
 import Flowstar.Parse
 import Flowstar.Translate
 import Process.QuickCheck
+import Process.Simplify
+import Test.QuickCheck
 
 checkFlowstar :: Double -> Double -> FilePath -> IO ()
 checkFlowstar delta dur file = do
   mod <- parseFile file
-  checkAssertionsIO delta dur (types mod) (model mod)
+  checkAssertionsIO delta dur (types mod) (simplify (model mod))
+
+checkFlowstarNM :: Double -> Double -> FilePath -> IO ()
+checkFlowstarNM delta dur file = do
+  mod <- parseFile file
+  quickCheck (checkAssertionsVal delta dur (types mod) (simplify (model mod)))
