97a36b0bce2ad2504e2f987aee6ec28357c71413
Nick Smallbone <nick@smallbone.se>
2020-02-07 13:21:32 +0100

    Fix Flowstar translation.

--- src/Flowstar/Translate.hs
+++ src/Flowstar/Translate.hs
@@ -102,13 +102,15 @@ model F.Model{..} =
       modeNum x = Map.findWithDefault undefined x modeNums
       start =
         P.set state (modeNum initialMode) P.&
-        P.par [P.set (P.Global x) (P.var (P.Global ("init_" ++ x))) | x <- Set.toList vars] P.&
-        P.par [P.assume "initial constraint" (constraint c) | c <- initialVars]
+        P.par [P.set x e | (x, e) <- subst] P.&
+        P.par [P.assume "initial constraint" (P.substitute subst (constraint c)) | c <- initialVars]
       step = foldr modeIte (P.assert "invalid mode" P.false) (Map.toList modes)
       modeIte (name, val) rest =
         P.ite (P.var state P.==? modeNum name)
           (mode (\name -> P.set state (modeNum name)) val)
           rest
+      subst =
+        [(P.Global x, P.var (P.Global ("init_" ++ x))) | x <- Set.toList vars]
     in P.process start step
 
 mode :: (F.ModeName -> P.Step) -> F.Mode -> P.Step
